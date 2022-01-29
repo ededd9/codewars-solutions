b729118ca46931773359642046b46e11c226b7cb
Edward Hurtado <edhur9@gmail.com>
2022-01-28 22:14:56 -0500

    day1 entries

--- solutions.js
+++ solutions.js
@@ -1,5 +1,9 @@
 //[8 KYU]
 //Abbreviate a Two Word Name
-// function abbrevName(name){
-//     return name.split(' ').map(word => word.charAt(0).toUpperCase()).join('.')
-//   }
+// function abbrevName(name) {
+//   return name
+//     .split(' ')
+//     .map(word => word.charAt(0).toUpperCase())
+//     .join('.');
+// }
+
